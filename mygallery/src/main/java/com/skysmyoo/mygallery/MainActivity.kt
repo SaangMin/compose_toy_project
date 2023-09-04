f74e1eacc48da724491a26cba3252e6a55b49f3e
skysmyoo7 <skysmyoo7@gmail.com>
2023-09-04 11:18:53 +0900

    화면 넘김 애니메이션 구현

--- mygallery/src/main/java/com/skysmyoo/mygallery/MainActivity.kt
+++ mygallery/src/main/java/com/skysmyoo/mygallery/MainActivity.kt
@@ -20,35 +20,34 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberImagePainter
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.HorizontalPagerIndicator
+import com.google.accompanist.pager.calculateCurrentOffsetForPage
 import com.google.accompanist.pager.rememberPagerState
-import com.skysmyoo.mygallery.ui.theme.ComposeToyProjectTheme
+import kotlin.math.absoluteValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val viewModel = viewModel<MainViewModel>()
             var granted by remember { mutableStateOf(false) }
 
             val launcher =
@@ -65,7 +64,8 @@ class MainActivity : ComponentActivity() {
             }
 
             if (granted) {
-                Text("권한 허용됨")
+                viewModel.fetchPhotos()
+                HomeScreen(photoUris = viewModel.photoUris.value)
             } else {
                 PermissionRequestScreen {
                     launcher.launch(Manifest.permission.READ_MEDIA_IMAGES)
@@ -137,7 +137,27 @@ fun HomeScreen(photoUris: List<Uri>) {
                 .padding(16.dp)
                 .fillMaxSize(),
         ) { pageIndex ->
-            Card() {
+            Card(
+                modifier = Modifier
+                    .graphicsLayer {
+                        val pageOffset = calculateCurrentOffsetForPage(pageIndex).absoluteValue
+
+                        lerp(
+                            start = 0.85f,
+                            stop = 1f,
+                            fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                        ).also { scale ->
+                            scaleX = scale
+                            scaleY = scale
+                        }
+
+                        alpha = lerp(
+                            start = 0.5f,
+                            stop = 1f,
+                            fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                        )
+                    }
+            ) {
                 Image(
                     painter = rememberImagePainter(
                         data = photoUris[pageIndex],
@@ -157,3 +177,6 @@ fun HomeScreen(photoUris: List<Uri>) {
         )
     }
 }
+
+private fun lerp(start: Float, stop: Float, fraction: Float): Float =
+    (1 - fraction) * start + fraction * stop
