c2add56d55810df82e93b50e9301c0cf9647263c
skysmyoo7 <skysmyoo7@gmail.com>
2023-09-04 10:58:43 +0900

    사진 가져오는 기능 가진 뷰모델 생성

--- mygallery/src/main/java/com/skysmyoo/mygallery/MainActivity.kt
+++ mygallery/src/main/java/com/skysmyoo/mygallery/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.skysmyoo.mygallery
 
 import android.Manifest
+import android.app.Application
+import android.content.ContentUris
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
@@ -15,6 +19,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -24,6 +29,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
 import com.skysmyoo.mygallery.ui.theme.ComposeToyProjectTheme
 
 class MainActivity : ComponentActivity() {
@@ -67,5 +74,38 @@ fun PermissionRequestScreen(onClick: () -> Unit) {
         Button(onClick = { onClick() }) {
             Text("권한 요청")
         }
+    }
+}
+
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val _photoUris = mutableStateOf(emptyList<Uri>())
+    val photoUris: State<List<Uri>> = _photoUris
+
+    fun fetchPhotos() {
+        val uris = mutableListOf<Uri>()
+
+        getApplication<Application>().contentResolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            null,
+            null,
+            null,
+            "${MediaStore.Images.ImageColumns.DATE_TAKEN} DESC"
+        )?.use { cursor ->
+            val idIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID)
+
+            while (cursor.moveToNext()) {
+                val id = cursor.getLong(idIndex)
+
+                val contentUri = ContentUris.withAppendedId(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    id,
+                )
+
+                uris.add(contentUri)
+            }
+        }
+
+        _photoUris.value = uris
     }
 }
