9038893bc44a3732640589e0697f299a286e9fa9
skysmyoo7 <skysmyoo7@gmail.com>
2023-09-04 11:54:11 +0900

    UI 구현

--- levelmeasuring/src/main/java/com/skysmyoo/levelmeasuring/MainActivity.kt
+++ levelmeasuring/src/main/java/com/skysmyoo/levelmeasuring/MainActivity.kt
@@ -9,8 +9,16 @@ import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
@@ -67,4 +75,49 @@ class MainViewModel(application: Application) : AndroidViewModel(application),
         }
     }
 
+}
+
+@Composable
+fun TiltScreen(x: Float, y: Float) {
+
+    val yCoord = x * 20
+    val xCoord = y * 20
+
+    Canvas(modifier = Modifier.fillMaxSize()) {
+        val centerX = size.width / 2
+        val centerY = size.height / 2
+
+        // 바깥 원
+        drawCircle(
+            color = Color.Black,
+            radius = 100f,
+            center = Offset(centerX, centerY),
+            style = Stroke(),
+        )
+
+        // 녹색 원
+        drawCircle(
+            color = Color.Green,
+            radius = 100f,
+            center = Offset(xCoord + centerX, yCoord + centerY),
+        )
+
+        // 가운데 십자가
+        drawLine(
+            color = Color.Black,
+            start = Offset(centerX - 20, centerY),
+            end = Offset(centerX + 20, centerY),
+        )
+        drawLine(
+            color = Color.Black,
+            start = Offset(centerX, centerY - 20),
+            end = Offset(centerX, centerY + 20),
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewCircle() {
+    TiltScreen(x = 30f, y = 20f)
 }
