3016f74ce1c644750804cd8eb88c96e6277583fc
skysmyoo7 <skysmyoo7@gmail.com>
2023-09-04 11:45:44 +0900

    센서 서비스 기능 가진 ViewModel 생성

--- levelmeasuring/src/main/java/com/skysmyoo/levelmeasuring/MainActivity.kt
+++ levelmeasuring/src/main/java/com/skysmyoo/levelmeasuring/MainActivity.kt
@@ -1,46 +1,70 @@
 package com.skysmyoo.levelmeasuring
 
+import android.app.Application
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.skysmyoo.levelmeasuring.ui.theme.ComposeToyProjectTheme
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeToyProjectTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+class MainViewModel(application: Application) : AndroidViewModel(application),
+    LifecycleEventObserver, SensorEventListener {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeToyProjectTheme {
-        Greeting("Android")
+    private val _x = mutableStateOf(0f)
+    val x: State<Float> = _x
+
+    private val _y = mutableStateOf(0f)
+    val y: State<Float> = _y
+
+    private val sensorManager by lazy {
+        application.getSystemService(Context.SENSOR_SERVICE) as SensorManager
     }
+
+    private fun registerSensor() {
+        sensorManager.registerListener(
+            this,
+            sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+            SensorManager.SENSOR_DELAY_NORMAL,
+        )
+    }
+
+    private fun unregisterSensor() {
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        event?.let {
+            _x.value = event.values[0]
+            _y.value = event.values[1]
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+
+    override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+        if (event == Lifecycle.Event.ON_RESUME) {
+            registerSensor()
+        } else if (event == Lifecycle.Event.ON_PAUSE) {
+            unregisterSensor()
+        }
+    }
+
 }
