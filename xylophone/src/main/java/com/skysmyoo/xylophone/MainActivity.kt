cdc26ca7f16de0314f68a009b7ce5fbfe69ac0bf
skysmyoo7 <skysmyoo7@gmail.com>
2023-09-04 14:12:26 +0900

    사운드 재생 기능 구현

--- xylophone/src/main/java/com/skysmyoo/xylophone/MainActivity.kt
+++ xylophone/src/main/java/com/skysmyoo/xylophone/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.skysmyoo.xylophone
 
+import android.app.Application
+import android.media.SoundPool
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.lifecycle.AndroidViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +13,28 @@ class MainActivity : ComponentActivity() {
         setContent {
 
         }
+    }
+}
+
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+    private val soundPool = SoundPool.Builder().setMaxStreams(8).build()
+    private val sounds = listOf(
+        soundPool.load(application.applicationContext, R.raw.do1, 1),
+        soundPool.load(application.applicationContext, R.raw.re, 1),
+        soundPool.load(application.applicationContext, R.raw.mi, 1),
+        soundPool.load(application.applicationContext, R.raw.fa, 1),
+        soundPool.load(application.applicationContext, R.raw.sol, 1),
+        soundPool.load(application.applicationContext, R.raw.la, 1),
+        soundPool.load(application.applicationContext, R.raw.si, 1),
+        soundPool.load(application.applicationContext, R.raw.do2, 1),
+    )
+
+    fun playSound(index: Int) {
+        soundPool.play(sounds[index], 1f, 1f, 0, 0, 1f)
+    }
+
+    override fun onCleared() {
+        soundPool.release()
+        super.onCleared()
     }
 }
